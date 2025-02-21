6d187fa46b9c39b83defe9abaf49992f55ebe4e0
Menso1310 <mensur.cavak@edu.fit.ba>
2025-02-21 07:45:16 +0100

    Dodano rjesenje ispita 20.02.2025 grupa 1 (#24)

---
+++ Ispiti/2025-02-20/Rješenje/DLWMS.Data/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DLWMS.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string Email { get; set; }
+        public string BrojIndeksa { get; set; }
+        public string Lozinka { get; set; }
+        public int GradId { get; set; }
+        public int SpolId { get; set; }
+        public byte[] Slika { get; set; }
+        public bool Aktivan { get; set; }
+        public int SpolID { get; set; }
+        public Spol Spol { get; set; }
+        [NotMapped] public string IndeksImePrezime => $"({BrojIndeksa}) {Ime} {Prezime}";
+        [NotMapped] public string Naziv => $"({BrojIndeksa}) {Ime} {Prezime}";
+    }
+}
