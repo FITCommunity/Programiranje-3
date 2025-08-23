a719ab1044d066dc64755d153185ef96843da184
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-24 01:23:12 +0200

    Added placeni rok exam

---
+++ Ispiti/2025-07-03/Rjesenje/DLWMS.WinApp/IspitBrojIndeksa/frmPretragaBrojIndeksa.cs
@@ -0,0 +1,112 @@
+﻿using DLWMS.Data.IspitBrojIndeksa;
+using DLWMS.Infrastructure;
+using DLWMS.WinApp.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace DLWMS.WinApp.IspitBrojIndeksa
+{
+    public partial class frmPretragaBrojIndeksa : Form
+    {
+        private readonly DLWMSContext DB = SharedBrojIndeksa.DB;
+
+        public frmPretragaBrojIndeksa()
+        {
+            InitializeComponent();
+
+            dgvStudentiStipendije.AutoGenerateColumns = false;
+            cbGodina.SelectedIndex = 0;
+
+            LoadStipendijeGodineIntoComboBox();
+            LoadStudentiStipendijeIntoDgv();
+        }
+
+        private void LoadStipendijeGodineIntoComboBox()
+        {
+            var godina = cbGodina.SelectedItem.ToString();
+
+            cbStipendijaGodina.DataSource = DB.StipendijeGodine
+                              .Include(stipendijaGodina => stipendijaGodina.StipendijeBrojIndeksa)
+                              .Where(stipendijaGodina => stipendijaGodina.Godina.ToString() == godina)
+                              .ToList();
+        }
+
+        private void LoadStudentiStipendijeIntoDgv()
+        {
+            var godina = cbGodina.SelectedItem.ToString();
+            var stipendijaGodina = (StipendijeGodineBrojIndeksa)cbStipendijaGodina.SelectedItem;
+
+            var studentiStipendije = DB.StudentiStipendije
+                                 .Include(studentiStipendije => studentiStipendije.Studenti)
+                                 .Include(studentiStipendije=>studentiStipendije.StipendijeGodineBrojIndeksa)
+                                 .ThenInclude(stipendijaGodina => stipendijaGodina.StipendijeBrojIndeksa)
+                                 .Where(studentiStipendije => studentiStipendije.StipendijeGodineBrojIndeksa == stipendijaGodina)
+                                 .ToList();
+
+            dgvStudentiStipendije.DataSource = studentiStipendije;
+            Text = $"Broj prikazanih studenata: {studentiStipendije.Count}";
+
+            if (studentiStipendije.Count == 0) 
+            {
+                string nazivStipendije = (stipendijaGodina == null) ? 
+                    "Nema naziva za stipendiju" : stipendijaGodina.StipendijeBrojIndeksa.Naziv;
+
+                MessageBox.Show($"U bazi nisu evidentirani studenti kojima je u {godina}.godini dodijeljena {nazivStipendije}.");
+            }
+        }
+
+        private void ComboBoxGodinaSelectionChangeCommitted(object sender, EventArgs e)
+        {
+            LoadStipendijeGodineIntoComboBox();
+            LoadStudentiStipendijeIntoDgv();
+        }
+
+        private void ComboBoxStipendijaGodinaSelectionChangeCommitted(object sender, EventArgs e)
+            => LoadStudentiStipendijeIntoDgv();
+
+        private void DgvStudentiStipendijeCellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex != BtnUkloni.Index) 
+            {
+                return;
+            }
+
+            var studentStipendija = (StudentiStipendijeBrojIndeksa)dgvStudentiStipendije.GetOdabraniRed();
+            DialogResult dialogResult = MessageBox.Show("Da li ste sigurni da zelite obrisati stipendiju za odabranog studenta?", "Brisanje stipendije za studenta", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes) 
+            {
+                DB.Remove(studentStipendija);
+                DB.SaveChanges();
+                LoadStudentiStipendijeIntoDgv();
+            }
+        }
+
+        private void StudentiStipendijeDgvCellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+
+            var studentStipendija = (StudentiStipendijeBrojIndeksa)dgvStudentiStipendije.GetOdabraniRed();
+
+            using var formaAddEdit = new frmStipendijaAddEditBrojIndeksa(studentStipendija);
+            formaAddEdit.ShowDialog();
+            LoadStudentiStipendijeIntoDgv();
+        }
+
+        private void DodajStipendijuBtnClick(object sender, EventArgs e)
+        {
+            using var formaAddEdit = new frmStipendijaAddEditBrojIndeksa();
+            formaAddEdit.ShowDialog();
+            LoadStudentiStipendijeIntoDgv();
+        }
+
+        private void StipendijePoGodinamaBtnClick(object sender, EventArgs e)
+        {
+            var formaStipendijePoGodinama = new frmStipendijeBrojIndeksa();
+            formaStipendijePoGodinama.ShowDialog();
+            LoadStipendijeGodineIntoComboBox();
+        }
+    }
+}
---
+++ Ispiti/2025-07-03/Rjesenje/DLWMS.WinApp/IspitBrojIndeksa/frmStipendijaAddEditBrojIndeksa.cs
@@ -0,0 +1,109 @@
+﻿using DLWMS.Data;
+using DLWMS.Data.IspitBrojIndeksa;
+using DLWMS.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace DLWMS.WinApp.IspitBrojIndeksa
+{
+    public partial class frmStipendijaAddEditBrojIndeksa : Form
+    {
+        private readonly DLWMSContext DB = SharedBrojIndeksa.DB;
+        private readonly StudentiStipendijeBrojIndeksa _studentStipendija;
+        private readonly bool _isUpdate;
+
+        public frmStipendijaAddEditBrojIndeksa()
+        {
+            InitializeComponent();
+            InitializeFormData();
+
+            _studentStipendija = new StudentiStipendijeBrojIndeksa();
+            _isUpdate = false;
+        }
+
+        public frmStipendijaAddEditBrojIndeksa(StudentiStipendijeBrojIndeksa studentStipendija)
+        {
+            InitializeComponent();
+            InitializeFormData();
+
+            _studentStipendija = studentStipendija;
+            cbStudent.SelectedItem = studentStipendija.Studenti;
+            cbStudent.Enabled = false;
+
+            cbGodina.SelectedItem = studentStipendija.Godina.ToString();
+            LoadStipendijeGodineIntoComboBox();
+            cbStipendijaGodina.SelectedItem = studentStipendija.StipendijeGodineBrojIndeksa;
+
+            _isUpdate = true;
+        }
+
+        private void InitializeFormData()
+        {
+            LoadStudentiIntoComboBox();
+            cbGodina.SelectedIndex = 0;
+            LoadStipendijeGodineIntoComboBox();
+        }
+
+        private void LoadStudentiIntoComboBox()
+        {
+            var studenti = DB.Studenti.ToList();
+
+            cbStudent.DataSource = studenti;
+        }
+
+        private void LoadStipendijeGodineIntoComboBox()
+        {
+            var godina = cbGodina.SelectedItem.ToString();
+
+            cbStipendijaGodina.DataSource = DB.StipendijeGodine
+                              .Include(stipendijaGodina => stipendijaGodina.StipendijeBrojIndeksa)
+                              .Where(stipendijaGodina => stipendijaGodina.Godina.ToString() == godina)
+                              .ToList();
+        }
+
+        private void GodinaComboBoxSelectionChangeCommitted(object sender, EventArgs e)
+            => LoadStipendijeGodineIntoComboBox();
+
+        private bool DaLiStudentStipendijaVecPostoji(
+            Student student,
+            StipendijeGodineBrojIndeksa stipendijaGodina
+            ) => DB.StudentiStipendije
+                   .Include(studentStipendija => studentStipendija.Studenti)
+                   .Include(studentStipendija => studentStipendija.StipendijeGodineBrojIndeksa)
+                   .Any(studentStipendija => 
+                      studentStipendija != _studentStipendija 
+                   && studentStipendija.Studenti == student 
+                   && studentStipendija.StipendijeGodineBrojIndeksa == stipendijaGodina);
+
+        private void SacuvajBtnClick(object sender, EventArgs e)
+        {
+            var student = (Student)cbStudent.SelectedItem;
+            var stipendijaGodina = (StipendijeGodineBrojIndeksa)cbStipendijaGodina.SelectedItem;
+
+            if (stipendijaGodina == null)
+            {
+                MessageBox.Show("Niste odabrali validnu stipendiju", "Greska");
+                return;
+            }
+            else if (DaLiStudentStipendijaVecPostoji(student, stipendijaGodina))
+            {
+                MessageBox.Show("Student ima vec odabranu stipendiju", "Greska");
+                return;
+            }
+
+            _studentStipendija.Studenti = student;
+            _studentStipendija.StipendijeGodineBrojIndeksa = stipendijaGodina;
+
+            if (_isUpdate) 
+            {
+                DB.Update(_studentStipendija);
+            }
+            else 
+            {
+                DB.Add(_studentStipendija);
+            }
+
+            DB.SaveChanges();
+            Close();
+        }
+    }
+}
